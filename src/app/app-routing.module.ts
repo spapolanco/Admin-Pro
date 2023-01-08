aa5252cdf7a6e072f95e9b11896ccbb49930b895
spapolanco <spapolanco@gmail.com>
2023-01-08 02:44:42 -0500

    The End of Design Admin Pro Framework - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+
+// Modulos 
+// [módulos prefiero que estén siempre arriba porque 
+// son digamos agrupadores de otro montón de componentes.]
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
 
 
 const routes: Routes = [
-  {path:'',
-  component: PagesComponent,
-  children:[
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Grafica1Component},
-    {path:'', redirectTo:'/dashboard',pathMatch:'full'},
-  ]
 
-  },
+// path:'/dashboard' PagesRouting
+// path:'/auth' AuthRouting
+// path:'/medicos' medicosRouting
+// path:'/compras' comprasRouting
 
 
-
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-   
- 
-  {path:'**', component:NopagefoundComponent},
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent },
 
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes),
+  //   // imports: [RouterModule.forRoot(routes, {useHash:true})],
+  // PagesRoutingModule,
+  // AuthRoutingModule
+
+  imports: [RouterModule.forRoot(routes, {useHash:true}),PagesRoutingModule,AuthRoutingModule],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
